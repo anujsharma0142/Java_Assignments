7558a2acd8cbc6651f264f3a95754310e0a6a0a6
ANUJ SHARMA <54332314+imanujsharma99@users.noreply.github.com>
2023-01-18 09:55:19 +0530

    updated code

--- BooKCricket.java
+++ BooKCricket.java
@@ -1,48 +1,93 @@
 package com.monocept.test;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class BookCricket {
 		public static void main(String[] args) {
 			
-			int player1 = 0;
-			int player2 = 0;
-			int turn1 = 0;
-			int turn2 = 0;
-			
-			Random ran = new Random();
 			while(true) {
-				turn1++;
-				int temp = ran.nextInt(300)%7;
-				if(temp==0)
-					break;
-				player1+=temp;
-			}
-			
-			while(true) {
-				turn2++;
-				int temp=ran.nextInt(300)%7;
-				if(temp==0);
-				break;
-				player2+=temp;
+				Scanner input = new Scanner(System.in);
+				int score1 = 0;
+				int score2 = 0;
+				int turn1 = 0;
+				int turn2 = 0;
 				
-			}
-			
-			//check high score
-			if(player1>player2) {
-				System.out.println("Player 1 won the game and the player1 value is " + player1);
-			}
-			if(player1>player2) {
-				System.out.println("Player 2 won the game and the player2 value is " + player2);
-			}
-			
-			// check lesser turns
-			else if(turn1<turn2) {
-				System.out.println("Player 1 won the game and the turn1 value is " + turn1);
-			}
-			
-			else if(turn1>turn2) {
-				System.out.println("Player 2 won the game the turn2 value is " + turn2);
+				System.out.println("For start press 1 and for end press 0 : ");
+				int x = input.nextInt();
+				if(x==0)
+					break;
+				input.nextLine();
+				System.out.println("Name for player 1 : ");
+				String st1 = input.nextLine();
+				
+				System.out.println("Name for player 2: ");
+				String st2 = input.nextLine();
+				
+				System.out.println("Player : " + st1);
+				
+				Random var  = new Random();
+				while(true) {
+					System.out.println("Enter 1 for open Book : ");
+					int data = input.nextInt();
+					if(data==0) break;
+					else {
+						int temp = var.nextInt(300);
+						turn1++;
+						int rem = temp%7;
+						score1 += rem;
+						System.out.println("Page Number : " + temp +  temp%7  + " ");
+						System.out.println("Score : " + score1 );
+						if(rem ==0) break;
+					}
+					
+				}
+				
+				System.out.println("Player1 "+ " Total Score : " + score1 );
+				System.out.println("Player1 "+ "Takes " + turn1 + "Rounds ");
+				System.out.println("Neeeds "+ score1+1 + "Score in "+ turn1 + "Rounds to Winn Match");
+				
+				System.out.println("Player : " + st2);
+				
+				int flag = turn1;
+				while(true) {
+					System.out.println("Open book for press 1 :");
+					int data = input.nextInt();
+					if(data==0) break;
+					else {
+						int temp = var.nextInt(300);
+						int rem = temp%7;
+						score2 += rem;
+						flag--;
+						turn2++;
+						System.out.println("Page No- " + temp + " ");
+						System.out.println("Points : " + temp%7 + " ");
+						score2+=temp%7;
+						System.out.println("Score : " + score2 + " ");
+						System.out.println("Remaining Rounds : " + flag);
+						
+						if(flag == 0 && score1>score2) {
+							System.out.println(st1 + "Winn the Match ");
+							break;
+						}
+						if(score2>score1) {
+							System.out.println(st2 + " Win the Match");
+							break;
+						}
+						
+						else if(score2 == score1) {
+							if(turn1>turn2) {
+							System.out.println(st2 + "Win Match");
+							break;
+							}
+							else if(turn1<turn2) {
+								System.out.println(st1 + "Win Match");
+								break;
+							}
+						}
+					}
+				}
+				
 			}
 
 	}
